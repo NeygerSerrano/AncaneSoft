d8dabe96fc6f37f0abea184a04ba8b5f8e7bf466
NeygerSerrano <neyger2001@gmail.com>
2026-09-10 10:10:52 -0500

    feat: implementacion fase 1 (SEO y Meta)

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('agendamiento.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="seo/robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="seo/sitemap.xml", content_type="application/xml")),
 ]
 
 if settings.DEBUG:
