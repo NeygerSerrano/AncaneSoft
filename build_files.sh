9e9d60e538c7ad6cc4d9b4b59e262c8c9fde83a4
NeygerSerrano <neyger2001@gmail.com>
2026-09-15 15:58:43 -0500

    fix: agregar break-system-packages para vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies..."
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
