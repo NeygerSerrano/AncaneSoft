8e8b01761cf811811dfb72c172ed3e92e2c18720
NeygerSerrano <neyger2001@gmail.com>
2026-09-15 16:19:37 -0500

    fix: borrar migrate y mantener break-system-packages

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-echo "Installing dependencies..."
 pip3 install -r requirements.txt --break-system-packages
-
-echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
-
-echo "Running migrations..."
-python3 manage.py migrate --noinput
